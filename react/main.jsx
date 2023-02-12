07f21d45b7ea2356bfedf57420c967a0fc7c16d3
yuki-akiba-jp <rabuafe@gmail.com>
2023-02-12 17:49:04 +0900

    del react

--- react/main.jsx
+++
@@ -1,8 +0,0 @@
-function Title(props) {
-  return <h1>{props.title}</h1>;
-}
-
-const title = <Title title="Fruits List" />;
-document.getElementById("root").append(`<h1>hello</h1>`);
-
-ReactDOM.render(title, document.getElementById("root"));
